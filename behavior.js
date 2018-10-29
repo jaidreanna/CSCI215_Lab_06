46ee3eda19700ce0ec952cad78049f1cca9cff1f
jaidreanna <42620749+jaidreanna@users.noreply.github.com>
2018-10-29 19:36:15 -0400

    Add files via upload
    final commit

---
+++ behavior.js
@@ -0,0 +1,36 @@
+
+var re = new RegExp('<');
+
+if(re.test('name'))  {
+
+
+    var RegExp = onkeypress.style.backgroundColor='red';
+
+
+} else {
+
+    regExp = ('<'); onkeypress.style.backgroundColor ='white';
+
+}
+
+
+if(new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$').test(age.value));
+{
+
+    document.getElementById('age').style.backgroundColor = 'white';
+}
+else
+{
+    ('age').onkeypress = checkAge();
+
+}
+
+
+var regExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+
+var email = "jaidreanna@gmail.com";
+
+
+
+
+
