d21056b5d53d1a0fd344e263793990a07a0a293b
jaidreanna <42620749+jaidreanna@users.noreply.github.com>
2018-12-11 15:00:47 -0500

    Add files via upload
    final commit as of 12/11/18

--- action.js
+++ action.js
@@ -3,13 +3,45 @@ function checkEmail(email) {
     return re.test(email);
 
 }
+document.getElementById('name').onkeyup = function {
+
+    var regEx = new RegExp('<');
+    if(regExp.test(document.getElementbyId('name').value)) {
+
+        name.style.backgroundColor = 'red';
+    } else {
+        name.style.backgroundColor = '';
+    }
+};
+
+
 
 function checkAge() {
     var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
-    
-    if() {
+    var age = document.getElementById('age').value;
+    if(regEx.test(age)) {
+    console.log("contains invalid characters" + document.getElementById('age').value;
+    document.getElementById("age").style.backgroundColor = "red";
+
        
     } else {
-       
+       document.getElementById("age").style.backgroundColor = ''
     }
 }
+
+function validate (){
+    if (checkEmail(document.getElementById("email").value)){
+        alert("Check Email");
+        var inputs = document.getElementsByClassName('input');
+        for (var i = 0; i <inputs.length; i++) {
+            inputs[i].value = '';
+
+        }
+    }else {
+        alert("Failed to Validate. Please check enters for information.")
+    }
+}
+
+var regEx = "";
+
+
